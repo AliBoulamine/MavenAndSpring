f430a46fa3be42583de690054179cf183b7c13ba
Ali Boulamine <aliboulamine1@gmail.com>
2022-10-09 14:56:20 +0000

    deuxieme commit

---
+++ src/main/java/com/aaeetii/projetmaven/Alien.java
@@ -0,0 +1,38 @@
+package com.aaeetii.projetmaven;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+	
+	private int age;
+	
+	private Laptop laptop;
+	
+	public Alien() {
+		System.out.println("Alien Object created.");
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+		System.out.println("Age assigned");
+	}
+	
+	public Laptop getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+
+	public void code() {
+		System.out.println("I am coding...");
+		laptop.compile();
+	}
+
+}
---
+++ src/main/java/com/aaeetii/projetmaven/Laptop.java
@@ -0,0 +1,8 @@
+package com.aaeetii.projetmaven;
+
+public class Laptop {
+
+	public void compile() {
+		System.out.println("Compiled");
+	}
+}
